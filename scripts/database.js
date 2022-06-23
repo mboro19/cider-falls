80c2fba23762be94d426a9a3256fc8dbfc96b3d1
David Stewart <mboro19@gmail.com>
2022-06-22 21:32:39 -0500

    second commit, added db and some css

---
+++ scripts/database.js
@@ -0,0 +1,133 @@
+export const database = [
+    
+    guests = [
+        
+    {
+
+        id: 1,
+        name: "Tom Hanks",
+        locationId: 5,
+
+    },
+    {
+
+        id: 2,
+        name: "Patrick Stewart",
+        locationId: 3,
+
+    },
+    {
+
+        id: 3,
+        name: "Cuba Gooding Jr",
+        locationId: 1,
+
+    },
+    {
+
+        id: 4,
+        name: "Mila Kunis",
+        locationId: 4,
+
+    },
+    {
+
+        id: 5,
+        name: "John Travolta",
+        locationId: 6,
+
+    },
+    {
+
+        id: 6,
+        name: "Jessica Biel",
+        locationId: 2,
+
+    }
+
+        ],
+    
+    
+    locations = [
+        
+    {
+
+        id:1,
+        name: "North West Section",
+        guestId: 3,
+        servicesId: null,
+        mainAttraction: "Lodge",
+        servicesOffered: "parking, information, picnicing"
+
+    },
+    {
+
+        id:2,
+        name: "Northern Section",
+        guestId: 6,
+        servicesId: null,
+        mainAttraction: "Lost Wolf Hiking",
+        servicesOffered: "hiking, picnicing, rock climbing"
+
+    },
+    {
+
+        id:3,
+        name: "North East Section",
+        guestId: 2,
+        servicesId: null,
+        mainAttraction: "Chamfort River",
+        servicesOffered: "rafting, canoeing, fishing"
+
+    },
+    {
+
+        id:4,
+        name: "South West Section",
+        guestId: 4,
+        servicesId: null,
+        mainAttraction: "Gander River",
+        servicesOffered: "fishing and hiking"
+
+    },
+    {
+
+        id:5,
+        name: "Southern Section",
+        guestId: 1,
+        servicesId: null,
+        mainAttraction: "Campgrouds",
+        servicesOffered: "information, lodging, parking"
+
+
+    },
+    {
+
+        id:6,
+        name: "South East Section",
+        guestId: 5,
+        servicesId: null,
+        mainAttraction: "Pine Bluff Trails",
+        servicesOffered: "hiking, picnicing, zip lines"
+
+    },
+
+    ],
+    
+    
+    services = [
+        
+    {
+
+        id: 1,
+        name: "",
+        guestId: null, 
+        sectionId: null,
+
+
+
+    },
+
+],
+
+]
